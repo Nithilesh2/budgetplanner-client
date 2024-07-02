ec95124f55489f010361e1cde5aba5906ab01262
nithilesh <sunnybnithilesh@gmail.com>
2024-07-02 11:06:59 +0530

    added date

--- src/components/expenseLi.jsx
+++ src/components/expenseLi.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import style from "../css/expensesLi.css"
+import React, { useEffect, useState } from "react"
 
 export default function ExpenseLi(props) {
+  const [date, setDate] = useState()
+
+  useEffect(() => {
+    const today = new Date().toISOString().split("T")[0]
+    setDate(today)
+  }, [])
+
+  const dateInputChange = (eve) => {
+    setDate(eve.target.value)
+  }
   return (
     <>
       <li>
@@ -11,12 +22,18 @@ export default function ExpenseLi(props) {
         </span>
 
         <span className="expenseDate">
-          {props.date.toLocaleString("default", {
+          {/* {props.date.toLocaleString("default", {
             dateStyle: "short",
             timeStyle: "short",
-          })}
+          })} */}
+          <input
+            className="expenseDate"
+            type="date"
+            value={date}
+            onChange={dateInputChange}
+          />
         </span>
       </li>
     </>
-  );
+  )
 }
